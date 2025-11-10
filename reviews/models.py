d4c773db7dcf9c025818473d2311c737f78a0a06
Elue Ezekiel <elueezekiel2005@gmail.com>
2025-11-11 00:39:01 +0100

    reviews model updated

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products.models import Product
 
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="reviews")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
     rating = models.IntegerField(default=0)
     title = models.CharField(max_length=255)
     content = models.TextField()
